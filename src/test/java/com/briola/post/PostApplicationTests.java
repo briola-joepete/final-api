b904be83eebb802311a9508bbad0f7b72a07d313
Pepexxxxx <briola.joepete@gmail.com>
2025-05-15 00:59:43 +0800

    first commit

---
+++ src/test/java/com/briola/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briola.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
